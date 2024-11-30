e6c421b48100f604a8c5fc8e09237dac4e6ede35
szymqn <koodziejskisz@outlook.com>
2024-12-01 00:45:18 +0100

    Added models register to admin

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Photo, Slider
+from .models import Photo, Slider, SliderPhoto, Color, Theme, UserProfile
 
 # Register your models here.
 admin.site.register(Photo)
 admin.site.register(Slider)
+admin.site.register(SliderPhoto)
+admin.site.register(Color)
+admin.site.register(Theme)
+admin.site.register(UserProfile)
--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Product
+from .models import Category, Product, Comment, Discussion, Event
 
 # Register your models here.
 
+admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(Comment)
+admin.site.register(Discussion)
+admin.site.register(Event)
