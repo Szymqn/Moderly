4af5ea7637e6e0ae74395cfbae552d1c47a3684f
szymqn <koodziejskisz@outlook.com>
2024-12-07 19:24:51 +0100

    Updated comment view logic

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product, Category
+from .models import Product, Category, Comment
 
 
 def product_list(request):
@@ -13,5 +13,20 @@ def product_list(request):
 
 
 def product_detail(request, product_id):
+    current_user = request.user if request.user.is_authenticated else None
     product = get_object_or_404(Product, id=product_id)
-    return render(request, 'products/product_detail.html', {'product': product})
+
+    all_comments = Comment.objects.all()
+    valid_comments = []
+
+    for comment in all_comments:
+        if (str(current_user.category) == str(product.category) or
+                current_user.is_admin is True or
+                comment.user == current_user or
+                comment.approved is True
+        ):
+            valid_comments.append(comment)
+
+    return render(request, 'products/product_detail.html',
+                  {'product': product,
+                   'comments': valid_comments})
